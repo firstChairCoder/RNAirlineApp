661c18d166f16abc3749f78b967ef470426f90af
firstChairCoder <md1qq7@gmail.com>
2022-04-24 07:43:37 +0100

    Extra additions and component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import type { TextInputProps } from "react-native";
+import { Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
+
+interface Props extends TextInputProps {
+  error?: string;
+  touched?: boolean;
+  label: string;
+  value: string;
+  mode: "flat" | "outlined" | undefined;
+}
+
+const InputField = ({
+  error,
+  touched,
+  onChangeText,
+  onBlur,
+  value,
+  placeholder,
+  label,
+  mode,
+}: Props) => (
+  <View>
+    <TextInput
+      onChangeText={onChangeText}
+      onBlur={onBlur}
+      value={value}
+      placeholder={placeholder}
+      label={label}
+      mode={mode}
+    />
+    <View
+      style={{
+        height: 26,
+        justifyContent: "center",
+      }}
+    >
+      {!!error && touched && <Text>{error}</Text>}
+    </View>
+  </View>
+);
+
+export default InputField;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as CircledButton } from "./CircledBtn";
 export { default as Header } from "./Header";
 export { default as FlightInfo } from "./FlightInfo";
 export { default as FlightData } from "./FlightData";
+export { default as InputField } from "./InputField";
